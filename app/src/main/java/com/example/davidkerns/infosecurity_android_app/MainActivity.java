e1d25013d90966ec4b81c9f249f4334aae183ecb
David Kerns <kernsdavida@gmail.com>
2016-03-17 18:05:44 -0400

    Initial commit

---
+++ app/src/main/java/com/example/davidkerns/infosecurity_android_app/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.davidkerns.infosecurity_android_app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button passwordGeneratorButton = (Button) findViewById(R.id.password_generator_button);
+        Button passwordValidatorButton = (Button) findViewById(R.id.password_validator_button);
+        Button fileAuthButton = (Button) findViewById(R.id.file_authenticator_button);
+
+        passwordGeneratorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, PasswordGeneratorActivity.class);
+                startActivity(i);
+            }
+        });
+
+        passwordValidatorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, PasswordValidatorActivity.class);
+                startActivity(i);
+            }
+        });
+
+        fileAuthButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getBaseContext(), FileAuthenticatorActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
