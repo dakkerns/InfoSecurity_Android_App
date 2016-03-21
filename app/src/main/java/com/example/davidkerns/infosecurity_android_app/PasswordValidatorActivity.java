cb279b1fb11d57b597470feffae8f3f053c601ce
gschoen <216962a>
2016-03-21 19:43:55 -0400

    added password criteria

--- app/src/main/java/com/example/davidkerns/infosecurity_android_app/PasswordValidatorActivity.java
+++ app/src/main/java/com/example/davidkerns/infosecurity_android_app/PasswordValidatorActivity.java
@@ -1,5 +1,6 @@
 package com.example.davidkerns.infosecurity_android_app;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class PasswordValidatorActivity extends AppCompatActivity {
@@ -32,6 +34,36 @@ public class PasswordValidatorActivity extends AppCompatActivity {
         String toastMessage = "Password: " + passField.getText().toString();
         Toast toast = Toast.makeText(getApplicationContext(), toastMessage, Toast.LENGTH_SHORT);
         toast.show();
+
+        TextView letter = (TextView) findViewById(R.id.haslet);
+        TextView number = (TextView) findViewById(R.id.hasnum);
+        TextView special = (TextView) findViewById(R.id.hasspec);
+        TextView criteria = (TextView) findViewById(R.id.criteriamet);
+
+        int metcount = 0;
+
+        if(toastMessage.matches(".*[a-zA-Z]+.*")) {
+            letter.setTextColor(Color.GREEN);
+            metcount++;
+        }
+        else
+            letter.setTextColor(Color.RED);
+
+        if(toastMessage.matches(".*\\d+.*")) {
+            number.setTextColor(Color.GREEN);
+            metcount++;
+        }
+        else
+            number.setTextColor(Color.RED);
+
+        if(toastMessage.matches(".*[!@#$%^&*]+.*")) {
+            special.setTextColor(Color.GREEN);
+            metcount++;
+        }
+        else
+            special.setTextColor(Color.RED);
+
+        criteria.setText("Met "+metcount+" of 3 safety criteria");
     }
 
 
