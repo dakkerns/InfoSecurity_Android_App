a35b77c7c10a2571ba1008b39c3016feb3086c6c
gschoen <216962a>
2016-03-21 17:56:29 -0400

    added input for password

--- app/src/main/java/com/example/davidkerns/infosecurity_android_app/PasswordValidatorActivity.java
+++ app/src/main/java/com/example/davidkerns/infosecurity_android_app/PasswordValidatorActivity.java
@@ -6,6 +6,9 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class PasswordValidatorActivity extends AppCompatActivity {
 
@@ -14,6 +17,23 @@ public class PasswordValidatorActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_password_validator);
 
+        Button password_valid = (Button) findViewById(R.id.validate_pword);
+
+        password_valid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showpass();
+            }
+        });
     }
+
+    void showpass(){
+        EditText passField = (EditText)findViewById(R.id.passfield);
+        String toastMessage = "Password: " + passField.getText().toString();
+        Toast toast = Toast.makeText(getApplicationContext(), toastMessage, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+
 
 }
